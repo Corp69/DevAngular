fdfec8cbe430022c7d88ad14ba987d92cffe20f3
TOTALPLAY\ecarballo <>
2023-07-12 12:33:53 -0600

    se Agrega el SweetAlert

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 import { AuthService } from './services/auth.service';
 @Component({
   selector: 'app-login',
@@ -41,4 +42,66 @@ export class LoginComponent  implements OnInit {
     });
     this.router.navigate(['./Control/inicio']);
   }
+
+
+
+  simpleAlert(){  
+    Swal.fire('Hello Angular');  
+  }  
+    
+  alertWithSuccess(){  
+    Swal.fire('Thank you...', 'You submitted succesfully!', 'success')  
+  }  
+  erroalert()  
+  {  
+    Swal.fire({  
+      icon: 'error',  
+      title: 'Oops...',  
+      text: 'Something went wrong!',  
+      footer: '<a href>Why do I have this issue?</a>'  
+    })  
+  }  
+  topend()  
+  {  
+    Swal.fire({  
+      position: 'top-end',  
+      icon: 'success',  
+      title: 'Your work has been saved',  
+      showConfirmButton: false,  
+      timer: 1500  
+    })  
+  }  
+  confirmBox(){  
+    Swal.fire({  
+      title: 'Are you sure want to remove?',  
+      text: 'You will not be able to recover this file!',  
+      icon: 'warning',  
+      showCancelButton: true,  
+      confirmButtonText: 'Yes, delete it!',  
+      cancelButtonText: 'No, keep it'  
+    }).then((result) => {  
+      if (result.value) {  
+        Swal.fire(  
+          'Deleted!',  
+          'Your imaginary file has been deleted.',  
+          'success'  
+        )  
+      } else if (result.dismiss === Swal.DismissReason.cancel) {  
+        Swal.fire(  
+          'Cancelled',  
+          'Your imaginary file is safe :)',  
+          'error'  
+        )  
+      }  
+    })  
+  }  
+
+
+
+
+
+
+
+
+
 }
