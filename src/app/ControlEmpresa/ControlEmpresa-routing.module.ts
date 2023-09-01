a6d6d7248b6e8716bdfbef6131a722719a2a9931
ingric <ingricardosg7@gmail.com>
2023-08-31 22:21:43 -0600

    message

--- src/app/ControlEmpresa/ControlEmpresa-routing.module.ts
+++ src/app/ControlEmpresa/ControlEmpresa-routing.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { SucursalComponent } from './sucursal/sucursal.component';
@@ -10,9 +9,9 @@ const routes: Routes = [
     path: '',
     component: HomeComponent,
     children: [
-      { path: 'Sucursal', component: SucursalComponent },
-      { path: 'Empresa', component: EmpresaComponent },
-      { path: '**', redirectTo: '/auth/login' }
+      { path: 'sucursal', component: SucursalComponent },
+      { path: 'empresa', component: EmpresaComponent },
+      { path: '**', redirectTo: '/ControlEmpresa/empresa' }
     ]
   }
 ];
--- src/app/ControlEmpresa/empresa/empresa.component.ts
+++ src/app/ControlEmpresa/empresa/empresa.component.ts
@@ -8,11 +8,6 @@ import { Component } from '@angular/core';
 export class EmpresaComponent {
 
 
-  foods = [
-    {value: 'steak-0', viewValue: 'Steak'},
-    {value: 'pizza-1', viewValue: 'Pizza'},
-    {value: 'tacos-2', viewValue: 'Tacos'},
-  ];
 
 
 
